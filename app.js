e4e22b1a3c642b72bc95681a54d883ee64901a84
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-06-26 15:57:08 +0530

    feat(express): Started express app to listen on PORT 3002

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 5001;
-
-app.get("/", (req, res) => {
-  res.status(200).send("I am a get Route");
-});
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`Server is Listening on PORT ${PORT}`);

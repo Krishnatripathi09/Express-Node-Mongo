fb94d0e0b7a8c9d8ba0daf40d6dc8cf80efcc936
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-06-28 17:43:38 +0530

    feat(Middleware): Created a Auth Middleware and called the next function to call the next function if user is verified

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const adminAuth = (req, res, next) => {
+  let token;
+
+  const authHeader = req.headers.Authorization || req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+
+    if (!token) {
+      res.status(401).json({ message: "Un-Authorized! Please Log-In Again" });
+    }
+    try {
+      const decodedMsg = jwt.verify(token, "MySecretToken#$6789");
+      req.user = decodedMsg;
+      next()
+    } catch (err) {
+      console.log("Error Occured :");
+    }
+  }
+};
